77c386be942eab5f51d64b23092ba45210753473
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-06-19 07:42:39 +0100

    Change site header

--- index.tsx
+++ index.tsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviePage";
 import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from "./components/siteHeader";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Link to="/movies/favourites">Favourites</Link>
         </li>
       </ul>
+       <SiteHeader />  
       <Routes>
         <Route path="/reviews/:id" element={<MovieReviewPage />} />
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
