bc01fe39d9b0b8bd00989b150715750055e62d80
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-06-27 07:13:19 +0100

    Add new review support

--- index.tsx
+++ index.tsx
@@ -10,6 +10,7 @@ import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -37,6 +38,7 @@ const App = () => {
         <MoviesContextProvider>
       <Routes>
         <Route path="/reviews/:id" element={<MovieReviewPage />} />
+        <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
