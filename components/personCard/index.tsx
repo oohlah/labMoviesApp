b1fb799728fff2605ae0e9ab0f6b6bfbcc9c9274
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-07-14 11:39:59 +0100

    added placehold 'more info' button and profile image

--- components/personCard/index.tsx
+++ components/personCard/index.tsx
@@ -6,10 +6,13 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import CardMedia from "@mui/material/CardMedia";
+import img from '../../images/film-poster-placeholder.png';
 
 const styles = {
   card: { maxWidth: 345 },
-  media: { height: 500 },
+  media: { height: 350 },
   avatar: {
     backgroundColor: "rgb(255, 0, 0)",
   },
@@ -32,7 +35,14 @@ const PersonCard: React.FC<PersonCardProps> = ({person}) => {
           </Typography>
         }
       />
-    
+    <CardMedia
+        sx={styles.media}
+        image={
+          person.profile_path
+            ? `https://image.tmdb.org/t/p/w500/${person.profile_path}`
+            : img
+        }
+      />
     <CardContent>
       <Grid container>
         <Grid item xs={6}>
@@ -41,6 +51,9 @@ const PersonCard: React.FC<PersonCardProps> = ({person}) => {
             {"character" in person ? person.character : person.job}
           </Typography>
         </Grid>
+        <Button variant="outlined" size="medium" color="primary">
+        More info ....
+        </Button>
       </Grid>
     </CardContent>
   </Card>
