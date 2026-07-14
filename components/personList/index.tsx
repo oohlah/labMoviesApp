c77972d7ead5cd57c0eb7556c0ca3c51253a2a5b
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-07-14 11:28:35 +0100

    fix: added grid container to structure grid items in PersonList

--- components/personList/index.tsx
+++ components/personList/index.tsx
@@ -4,13 +4,15 @@ import Grid from "@mui/material/Grid";
 import { PersonListProps } from "../../types/interfaces";
 
 const PersonList: React.FC<PersonListProps> = ({people}) =>{
-    const personCards = people.map((person) =>(
-          <Grid key={person.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
-          <PersonCard person={person}  />
-          </Grid>
-    ));
-     return personCards;
-
-}
-
+    return (
+          <Grid container spacing={2}>
+         {people.map((person) =>(
+          <Grid 
+           key={person.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
+           <PersonCard person={person} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
 export default PersonList;
