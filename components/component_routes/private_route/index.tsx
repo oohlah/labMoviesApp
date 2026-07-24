a397040bbcc39bfa7ec3e99ab53d76d207832e21
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-07-24 11:41:18 +0100

    feat: implemented supabase auth in private routes to navigate to login page for logged in user

--- components/component_routes/private_route/index.tsx
+++ components/component_routes/private_route/index.tsx
@@ -1,8 +1,27 @@
 import auth from "../../../auth_service";
 import {Navigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-const PrivateRoute = ({children}) => {
-    if(!auth.isUser) return (<Navigate to={'/loginPage'}/>)
+type PrivateRouteProps = {
+  children: React.ReactNode;
+}
+
+const PrivateRoute : React.FC<PrivateRouteProps>= ({children}) => {
+
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null); //empty boolean value
+    const location = useLocation();
+
+
+      useEffect(() => {
+        auth.isUser().then(setIsAuthenticated);  //if auth isUser set Authenticated to true
+    }, []);
+
+   
+      if (!isAuthenticated) {
+     return <Navigate to="/loginPage" state={{ from: location }} />;
+    }
+
     return (
         children
     )
