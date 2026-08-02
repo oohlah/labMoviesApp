d4b3fcb464b888a8aa08a5412c6fa83be1b820b5
Orla Fitzgerald <88854792+oohlah@users.noreply.github.com>
2026-08-02 08:39:41 +0100

    chore: made images optional on FantasyMovie

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -169,7 +169,7 @@ export interface FantasyCastMember {
   runtime: number;
   production_countries: string[];
   cast: FantasyCastMember[];
-  poster_path: string;
+  poster_path?: string;
   
 
 }
